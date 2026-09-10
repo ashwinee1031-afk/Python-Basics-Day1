4e8b945e5e74c2a52f7e6f22d29657c03afdee2f
Ashwini <ashwinee1031@gmail.com>
2026-09-10 15:26:54 +0530

    Python

--- demo.py
+++ demo.py
@@ -1,5 +1,13 @@
 #a= int(input("Enter a number"))
 #print("Number is odd:",a%2!=0)
 
-age = int(input("Enter age"))
-print(f"{age}years = {age*365}days")
+#age = int(input("Enter age"))
+#print(f"{age}years = {age*365}days")
+
+#min = int(input("Enter min"))
+#print(f"{min} is {min/60}hours {min%60}minutes")
+
+num = int(input("Enter a number"))
+print(f"{num} : last digit is {num%10}")
+
+
