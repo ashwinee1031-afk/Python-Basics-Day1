17aa5d5c88a67e58854cc750013d1581fa624ecb
Ashwini <ashwinee1031@gmail.com>
2026-09-10 22:48:04 +0530

    python

--- demo.py
+++ demo.py
@@ -7,7 +7,23 @@
 #min = int(input("Enter min"))
 #print(f"{min} is {min/60}hours {min%60}minutes")
 
-num = int(input("Enter a number"))
-print(f"{num} : last digit is {num%10}")
+#num = int(input("Enter a number"))
+#print(f"{num} : last digit is {num%10}")
+
+#swap numbers using arithmetic operators (a=10, b=20)
+#output a=10, b=20
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(f"After swapping numbers a is{a} and b is {b}")'''
+
+
+
+a = input("Enter your role")
+b = int(input("Enter your age "))
+print("Eligible ",a=="Student" and b <= 21)
+
 
 
