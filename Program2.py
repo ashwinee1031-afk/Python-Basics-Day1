1019715640f28ceda985e5a6e415610141a6b390
Ashwini <ashwinee1031@gmail.com>
2026-09-16 23:53:52 +0530

    Program

---
+++ Program2.py
@@ -0,0 +1,5 @@
+#calculate sum of all numbers from 1 to n in given list 
+a =[12,5,67,5,36]
+print(f"{sum(a)}")
+
+
---
+++ Program3.py
@@ -0,0 +1,7 @@
+#program to find average of the list
+
+z=[7,4,36,2,79]
+a= sum(z)
+avg = a/len(z)
+print(f"average of given list is{avg}")
+
